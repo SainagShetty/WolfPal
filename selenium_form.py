d156703d8cf1b303a7b593dbc76c13d155c84661
Rohan Chandavarkar <chandavarkar.rohan@gmail.com>
2018-04-05 18:14:00 -0400

    AutoSuggest Box done

--- selenium_form.py
+++ selenium_form.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 url = 'https://www.acs.ncsu.edu/php/coursecat/directory.php'
 
 driver = webdriver.Chrome()
@@ -6,3 +8,7 @@ driver = webdriver.Chrome()
 driver.get(url)
 
 driver.find_element_by_xpath("//select[@name='course-career']/option[text()='Graduate']").click()
+
+code = driver.find_element_by_id("auto-subject")
+code.send_keys("CSC")
+code.send_keys(Keys.ENTER)
