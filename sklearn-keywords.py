4a48e4629aa5594f383b080af4c5a3d8bb6ba588
neelkapadia <neel.kapadia10@gmail.com>
2018-04-07 16:59:28 -0400

    LDA using sklearn

---
+++ sklearn-keywords.py
@@ -0,0 +1,9 @@
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.decomposition import NMF, LatentDirichletAllocation
+
+no_features = 1000
+
+# NMF is able to use tf-idf
+tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
+tfidf = tfidf_vectorizer.fit_transform(documents)
+tfidf_feature_names = tfidf_vectorizer.get_feature_names()
