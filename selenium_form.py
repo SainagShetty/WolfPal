de7bdf7cb5a820d74dd17de53282fc2ccf9e2c21
Rohan Chandavarkar <chandavarkar.rohan@gmail.com>
2018-04-09 02:31:04 -0400

    Scraping Description done

--- selenium_form.py
+++ selenium_form.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from bs4 import BeautifulSoup
+import requests
+
 
 url = 'https://www.acs.ncsu.edu/php/coursecat/directory.php'
 
 driver = webdriver.Chrome()
 
 driver.get(url)
+
+
 
 driver.find_element_by_xpath("//select[@name='course-career']/option[text()='Graduate']").click()
 
@@ -17,5 +22,21 @@ code.send_keys("CSC")
 
 driver.find_element_by_id("subject-search-button").click()
 
-time.sleep(20)
+time.sleep(10)
 driver.find_element_by_link_text("CSC 506 - Architecture Of Parallel Computers").click()
+
+print driver.current_url
+page = requests.get(driver.current_url)
+#print page.text
+urlsoup = BeautifulSoup(page.text,"html.parser")
+
+description = driver.find_element_by_id("course-descr").text
+unit = driver.find_element_by_id("course-units").text
+title = driver.find_element_by_id("modalTitle")
+#print urlsoup.prettify()
+# print urlsoup
+
+# description = urlsoup.find("p",id="course-descr").text
+
+# print description
+print description
