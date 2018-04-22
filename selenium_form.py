2af7dd86c2df2816c943d17efca53bcb228894b5
Rohan Chandavarkar <chandavarkar.rohan@gmail.com>
2018-04-22 16:41:42 -0400

    After rollback

--- selenium_form.py
+++ selenium_form.py
@@ -23,13 +23,28 @@ code.send_keys("CSC")
 driver.find_element_by_id("subject-search-button").click()
 
 time.sleep(10)
-driver.find_element_by_link_text("CSC 506 - Architecture Of Parallel Computers").click()
+
+html_list = driver.find_element_by_id("course-search-results")
+items = html_list.find_elements_by_tag_name("li")
+# list_of_courses = []
+# fp = open("CSC-course-list.txt","w")
+# for item in items:
+#     text = item.text
+#     fp.write(text)
+#     fp.write('\n')
+# fp.close()
+
+#for item in items:
+driver.find_element_by_link_text(items[0].text).click()
 
 print driver.current_url
 page = requests.get(driver.current_url)
 #print page.text
 #durlsoup = BeautifulSoup(page.text,"html.parser")
 
+
+
+time.sleep(5)
 description = driver.find_element_by_id("course-descr").text
 unit = driver.find_element_by_id("course-units").text
 title = driver.find_element_by_id("modalTitle")
