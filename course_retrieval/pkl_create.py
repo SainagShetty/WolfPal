9f347b15a7ae037e6dbba2fb9fb160b3d1b9d72e
rohitnaik246 <rohit.naik246@gmail.com>
2018-04-24 01:22:00 -0400

    Remove app and course_retireval

--- course_retrieval/pkl_create.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-f = open('.cred.pkl', 'wb')
-username = "paylot"
-password = "wolfpal123"
-db_name = "wolfpal"
-collection_name = "courses" 
-pickle.dump((username, password, db_name, collection_name), f, protocol=pickle.HIGHEST_PROTOCOL)
-f.close()
